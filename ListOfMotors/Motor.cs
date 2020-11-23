de5f2117c8c7114f30e7c385742bf177803c9092
LukasCwaj <72606461+LukasCwaj@users.noreply.github.com>
2020-11-23 14:26:46 +0100

    Add project files.

---
+++ ListOfMotors/Motor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorList
+{
+    class Motor
+    {
+        public string name { get; set; }
+        public string engine;
+        int id;
+        static int nextId = 0;
+        public Motor(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            nextId++;
+            id = nextId;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+    }
+}
